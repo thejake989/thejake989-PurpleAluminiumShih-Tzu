9f5a27c1bca0eb6016a175b6f4b908881a04af67
Leonardo <josue.aguilar1995@gmail.com>
2022-06-23 16:47:20 -0500

    added Votes.js module and associations

--- models/index.js
+++ models/index.js
@@ -1,9 +1,14 @@
 const User = require("./User");
 const Poll = require("./Poll");
 const Choices = require("./Choices");
+const Vote = require("./Votes");
 
 // User associations
 User.hasMany(Poll, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Vote, {
   foreignKey: "user_id",
 });
 
@@ -16,7 +21,30 @@ Poll.hasMany(Choices, {
   foreignKey: "poll_id",
 });
 
+Poll.hasMany(Vote, {
+  foreignKey: "poll_id",
+});
+
 // Choices associations
 Choices.belongsTo(Poll, {
   foreignKey: "poll_id",
 });
+
+Choices.hasMany(Vote, {
+  foreignKey: "choices_id",
+});
+
+// Votes associations
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Poll, {
+  foreignKey: "poll_id",
+});
+
+Vote.belongsTo(Choices, {
+  foreignKey: "choices_id",
+});
+
+module.exports = { User, Poll, Choices, Vote };
