25e1675f5a8d3e7810347db935701a1a949f4de4
carlosrrdev <rr.carlos.dev@gmail.com>
2022-06-23 20:06:21 -0500

    Establish session cookie

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const express = require("express");
 const sequelize = require("./config/connection");
 const path = require("path");
 const routes = require("./controllers");
+const session = require("express-session");
 
 // Create express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Establish session
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 // Express middleware
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -27,7 +40,6 @@ app.get("/test", (req, res) => {
 app.get("/homepage", (req, res) => {
   res.render("homepage");
 });
-
 
 app.use(routes);
 
