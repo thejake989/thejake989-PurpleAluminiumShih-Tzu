cb5b0543dd35c782357bee2feec22c6e43a021e3
Jake <thejakethree989@gmail.com>
2022-06-25 14:35:22 -0500

    login page with handlebars

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,9 +4,11 @@ const { Poll, User, Vote, Choices } = require("../models");
 const withAuth = require("../utils/auth");
 
 // Render dashboard page
-router.get("/", withAuth, (req, res) => {
+router.get("/", (req, res) => {
   // Get data from database
   res.render("dashboard");
 });
 
 module.exports = router;
+
+//If not working apply the get route - withAuth,
