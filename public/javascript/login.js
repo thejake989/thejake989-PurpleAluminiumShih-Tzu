e62d13c4fa78e69e0e56e2400cbe97ec126c8add
samschool789@gmail.com <samschool789@gmail.com>
2022-06-25 20:27:55 -0500

    login and signup page

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -26,7 +26,8 @@
 
     async function loginFormHandler(event) {
         event.preventDefault();
-      
+        
+        
         const email = document.querySelector('#email-login').value.trim();
         const password = document.querySelector('#password-login').value.trim();
       
